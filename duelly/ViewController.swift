5119e6d3f1197ad0c9fad3442069f91bc95a5854
andrewschreiber <andrew.schreiber1@gmail.com>
2016-06-16 23:53:11 -0700

    Example API usage

--- duelly/ViewController.swift
+++ duelly/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        super.view.backgroundColor = UIColor.yellowColor()
-    }
+    
+//    var leagues:[League] = []
+//    
+//    var tableView:UITableView
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//        super.view.backgroundColor = UIColor.yellowColor()
+//        
+//        
+//        LeagueManager.sharedInstance.getLeagues { leagues in
+//            self.leagues = leagues
+//            self.tableView.reloadData()
+//        }
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return leagues.count
+//    }
+//    
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let league = leagues[indexPath.row]
+//        let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as! LeagueTableViewCell
+//        cell.leagueName = league.name
+//        cell.leagueDescription = league.description
+//        return cell
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
