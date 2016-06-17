0a3fe8cb9a191c32c961694d824ac42584b577bf
andrewschreiber <andrew.schreiber1@gmail.com>
2016-06-16 23:36:55 -0700

    validate merge

--- duelly/CurrentLeagueViewController.swift
+++ duelly/CurrentLeagueViewController.swift
@@ -1,28 +1,19 @@
 //
-//  MatchesViewController.swift
+//  CurrentLeagueViewController.swift
 //  duelly
 //
-//  Created by Hi_Hu on 9/28/15.
-//  Copyright © 2015 hi_hu. All rights reserved.
+//  Created by Mike Hu on 6/16/16.
+//  Copyright © 2016 hi_hu. All rights reserved.
 //
 
 import UIKit
-import GameplayKit
 
-class MatchesViewController: UIViewController {
-    
-    @IBOutlet weak var thisView: UIView!
+class CurrentLeagueViewController: UIViewController {
 
-    var dice = Dice()
-    let dice6 = GKRandomDistribution.d6()
-    
     override func viewDidLoad() {
-        thisView.layer.cornerRadius = 90
-        thisView.backgroundColor = UIColor.blackColor()
-        
         super.viewDidLoad()
 
-        dice.addToView(thisView)
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,10 +21,6 @@ class MatchesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func rollDidPress(sender: AnyObject) {
-        let randomNum = dice6.nextInt()
-        dice.rollDie(randomNum)
-    }
 
     /*
     // MARK: - Navigation
